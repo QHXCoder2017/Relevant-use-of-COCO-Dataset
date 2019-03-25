98f71963526c23fa4925bfa2f75fe9df1f6f2022
Huixiang Qiao <369861826@qq.com>
2019-03-25 22:44:56 +0800

    Update h5_observe.py

--- h5_observe.py
+++ h5_observe.py
@@ -1,7 +1,7 @@
 import h5py
 
-f = h5py.File('path/filename.h5', 'r')
 
+f = h5py.File('path/filename.h5', 'r')
 for k in f.keys():
     print(k)
 
@@ -11,4 +11,3 @@ p = f['part']
 print(b.shape)
 print(i.shape)
 print(p.shape)
-
