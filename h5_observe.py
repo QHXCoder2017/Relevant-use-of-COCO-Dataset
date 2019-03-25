51ab16a04efc0a2c2ce41d4cd54cde838fed3a59
Huixiang Qiao <369861826@qq.com>
2019-03-25 22:42:59 +0800

    Add files via upload

---
+++ h5_observe.py
@@ -0,0 +1,14 @@
+import h5py
+
+f = h5py.File('path/filename.h5', 'r')
+
+for k in f.keys():
+    print(k)
+
+b = f['bndbox']
+i = f['imgname']
+p = f['part']
+print(b.shape)
+print(i.shape)
+print(p.shape)
+
